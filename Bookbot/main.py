247854f734e39b38219d0dd9a0eb42665fbe4be2
Depigee <depigee@gmail.com>
2026-03-13 15:36:45 -0700

    Counted Characters

--- Bookbot/main.py
+++ Bookbot/main.py
@@ -1,10 +1,13 @@
-from stats import word_count
+from stats import word_count, count_characters
 
 def main():
     path_to_book = "books/frankenstein.txt"
     text = get_book_text(path_to_book)
     num_words = word_count(text)
     print(f"Found {num_words} total words")
+    counted_characters = count_characters(text)
+    print(counted_characters)
+
 
 def get_book_text(file):
     with open(file) as f:
--- Bookbot/stats.py
+++ Bookbot/stats.py
@@ -1,4 +1,14 @@
-
 def word_count(book_contents):
     split_words = book_contents.split()
     return len(split_words)
+
+def count_characters(text):
+    word_count_dict = {}
+        
+    lower_cased_chars = text.lower()
+    for char in lower_cased_chars:
+        if(char in word_count_dict):
+            word_count_dict[char] += 1
+        else:
+            word_count_dict[char] = 1
+    return word_count_dict
