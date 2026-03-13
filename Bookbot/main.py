732495b65bf15918a34763585e6f1e0e39c26301
Depigee <depigee@gmail.com>
2026-03-13 13:59:26 -0700

    Opened and Read File in

--- Bookbot/main.py
+++ Bookbot/main.py
@@ -1,1 +1,13 @@
-print("greetings boots")
+def main():
+    path_to_book = "books/frankenstein.txt"
+    text = get_book_text(path_to_book)
+    print(text)
+
+def get_book_text(file):
+    with open(file) as f:
+        file_contents = f.read()
+    return file_contents
+
+main()
+
+
