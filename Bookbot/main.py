feb93df15f8fd183ab9d246e6231e64669789804
Depigee <depigee@gmail.com>
2026-03-16 13:37:16 -0700

    counted characters

--- Bookbot/main.py
+++ Bookbot/main.py
@@ -1,18 +1,33 @@
-from stats import word_count, count_characters
+from stats import word_count, count_characters, sorted_list
 
 def main():
     path_to_book = "books/frankenstein.txt"
     text = get_book_text(path_to_book)
     num_words = word_count(text)
-    print(f"Found {num_words} total words")
     counted_characters = count_characters(text)
-    print(counted_characters)
 
+    sorted_dict = sorted_list(counted_characters) 
+
+    print_report(path_to_book, num_words, sorted_dict)
 
 def get_book_text(file):
     with open(file) as f:
         file_contents = f.read()
     return file_contents
+
+def print_report(path_to_book, num_words, sorted_dict):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path_to_book}")
+    print("----------- Word Count ----------")
+    print(f"{num_words} Found")
+    print("--------- Character Count -------")
+
+    for item in sorted_dict:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item['char']}: {item['num']}")
+
+    print("============= END ===============")
 
 main()
 
--- Bookbot/stats.py
+++ Bookbot/stats.py
@@ -4,11 +4,27 @@ def word_count(book_contents):
 
 def count_characters(text):
     word_count_dict = {}
-        
     lower_cased_chars = text.lower()
+    
     for char in lower_cased_chars:
         if(char in word_count_dict):
             word_count_dict[char] += 1
         else:
             word_count_dict[char] = 1
     return word_count_dict
+
+def sort_on(dict):
+    return dict["num"]
+
+def sorted_list(dict):
+    sorted_list = []
+
+    for d in dict:
+        sorted_list.append({"char" : d, "num": dict[d]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list    
+
+
+    
+
+   
