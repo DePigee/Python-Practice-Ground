b1f23c1ad0b38d76e827d54f57b6e7c37d9546df
Depigee <depigee@gmail.com>
2026-03-16 13:49:30 -0700

    added more books

--- Bookbot/main.py
+++ Bookbot/main.py
@@ -1,7 +1,14 @@
 from stats import word_count, count_characters, sorted_list
+import sys
+
+
 
 def main():
-    path_to_book = "books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    path_to_book = sys.argv[1]
+
     text = get_book_text(path_to_book)
     num_words = word_count(text)
     counted_characters = count_characters(text)
