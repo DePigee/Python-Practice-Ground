761efdd72dd160e49d22e2cb90f7a047784bf33e
DePigee <77212209+DePigee@users.noreply.github.com>
2026-03-23 16:03:26 -0700

    added test scores

--- ParseScoreSafely/main.py
+++ ParseScoreSafely/main.py
@@ -17,7 +17,13 @@ def parse_scores(raw_scores):
 
     return valid_scores
 
+print(parse_scores(["10", "20", "30"]))
+# [10, 20, 30]
 
+print(parse_scores(["10", "oops", "25", "-5", None]))
+# "oops" and None are ignored (cannot be converted)
+# -5 is ignored (negative)
+# [10, 25]
 
-        
-        
+print(parse_scores(["abc", None, "-3"]))
+# raises ValueError("no valid scores")
